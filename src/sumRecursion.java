ec8dc697796b2eb96e09fed4e016cba148f33f3b
Shaurya Shekhar Srivastav <srivastashaurya@gmail.com>
2026-04-14 16:47:49 +0530

    Added Recursion logic for Sum and Fibonacci

---
+++ src/sumRecursion.java
@@ -0,0 +1,16 @@
+public class sumRecursion {
+    public static int getSum(int n) {
+
+//        if(n==1){
+//            return 1;
+//        }
+//        return n+(n-1);
+//
+        return (n == 1) ? 1 : n + getSum(n - 1);
+    }
+
+    public static void main(String[] args){
+//        int result = getSum(6);
+            System.out.println(getSum(6));
+    }
+}
